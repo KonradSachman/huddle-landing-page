c5589b998aa4393bafe364608659cd39a6ba674a
KonradSachman <konradsjg@gmail.com>
2020-11-16 22:38:36 +0100

    final changes after marge footer and cards branches to master brachn

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,7 @@ const browserSync = require('browser-sync').create();
 
 function compileSass(done) {
   gulp
-<<<<<<< HEAD
-    .src('./scss/footer.scss', { allowEmpty: true })
-=======
-    .src('./scss/footer.scss', { allowEmpty: true })
->>>>>>> cards
+    .src('./scss/main.scss', { allowEmpty: true })
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
     .pipe(autoprefixer())
